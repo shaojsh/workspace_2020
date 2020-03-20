b7bef1930bd22d4212cdd35c6bb0a5305a7f4ff6
junshuai shao <shaojunshuai2019@sina.com>
2020-03-21 07:30:38 +0800

    数据迁移-置空 --empty
    Signed-off-by: junshuai shao <shaojunshuai2019@sina.com>

--- user/models.py
+++ user/models.py
@@ -1,15 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     class Meta:
         db_table = 'user'
-    id = models.AutoField(primary_key = True)
-    name = models.CharField(max_length=48,null=False)
-    email = models.CharField(max_length=64,null=False,unique = True)
-    password = models.CharField(max_length=128,null=False)
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=48, null=False)
+    email = models.CharField(max_length=64, null=False, unique=True)
+    password = models.CharField(max_length=128, null=False)
 
     def __repr__(self):
         return "".format(self.id, self.name)
+
+    __str__ = __repr__
+
+
+class BlogPost(models.Model):
+    class Meta:
+        db_table = 'BlogPost'
+
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=1, null=False)
+    blogNet = models.CharField(max_length=148, null=False)
+    des = models.CharField(max_length=500, null=False)
+
+    def __repr__(self):
+        return "".format(self.id)
 
     __str__ = __repr__
--- user/views.py
+++ user/views.py
@@ -66,8 +66,8 @@ def blogPost(request: HttpRequest):
 def uploadFiles(request: HttpRequest):
     if request.method == "POST":  # 请求方法为POST时，进行处理
         myFile = request.FILES.get("myfile", None)  # 获取上传的文件，如果没有文件，则默认为None
-        if not myFile:
-            return HttpResponse("no files for upload!")
+        # if not myFile:
+        #     return HttpResponse("no files for upload!")
         destination = open(os.path.join("E:\\upload", myFile.name), 'wb+')  # 打开特定的文件进行二进制的写操作
         for chunk in myFile.chunks():  # 分块写入文件
             destination.write(chunk)
